324437b93b70784df8ec560615f0fb11499ac4e9
Sergei <novitskov@gmail.com>
2016-06-03 11:16:24 +0300

    login, pass сделать поля с проверкой и кнопку Auth, которая проверяет.

---
+++ js/index.js
@@ -0,0 +1,4 @@
+alert( "Привет, Мир!" );
+document.getElementById("nav").innerHTML="Этот текст вывел javaScript";
+document.getElementById("article").innerHTML="Этот текст статьи....";
+document.getElementById("button").ondblckick  = function() {alert('выбрали кнопку')}
