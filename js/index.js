67ba63c16131e6eddd43963115ff0e19d3d64e71
Sergei <novitskov@gmail.com>
2016-06-07 13:49:38 +0300

    auth login pass + javascript 07.06

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
-alert( "Привет, Мир!" );
 document.getElementById("nav").innerHTML="Этот текст вывел javaScript";
 document.getElementById("article").innerHTML="Этот текст статьи....";
 document.getElementById("button").ondblckick  = function() {alert('выбрали кнопку')}
+
+document.getElementById("submit").onclick=function(event)
+{
+	return validate();
+}
+
+function validate(form)
+{
+	var login = document.getElementById("text1")
+	var pass = document.getElementById("text2")
+	var flag = false;
+    if(login.value == "")
+    {   
+    	login.className = "ks1";
+    }
+    else if (pass.value == "")
+    {
+        pass.className = "ks1";
+    }
+}
